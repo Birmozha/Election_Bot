3a15999bf99766fe9aa7e8189411af41165d0f27
Birmozha <birmozha@gmail.com>
2023-04-18 21:17:04 +0500

    Trying something new

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
-from handlers.commands import cmd_start
-from handlers.complain import ComplainStates, define_category, wait_for_category
+from collections import namedtuple
 import os
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters import Text
+from sqlalchemy import select
 
+from database.database import Tree, Data, session
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -23,9 +24,131 @@ MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 bot = Bot(TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-dp.register_message_handler(cmd_start, commands=['start'], state=['*'])
-dp.register_callback_query_handler(wait_for_category, state=ComplainStates.choose_categry)
-dp.register_callback_query_handler(define_category)
+# STATE GROUPS --------------------------------------------------------------
+class InfoStates(StatesGroup):
+    dialog = State()
 
-def start_bot(dp):
-    executor.start_polling(dp, skip_updates=True)
+class ComplainStates(StatesGroup):
+    choose_categry = State()
+    additionals = State()
+
+
+# ---------------------------------------------------------------------------
+
+
+# FUNCTIONS -----------------------------------------------------------------
+
+def find_next(id=None) -> dict:
+    id = session.scalar(select(Tree.qid).where(Tree.pid == id))
+    text = session.scalar(select(Data.text).where(
+        Data.id == id)).split('<delimetr>')
+    next = session.scalars(select(Tree.qid).where(Tree.pid == id)).all()
+    final = {'id': id, 'text': text, 'next': next}
+    return final
+
+def find_keyboard(id) -> ReplyKeyboardMarkup | InlineKeyboardMarkup:
+    keyboard_type = session.scalar(
+        select(Tree.properties).where(Tree.qid == id)).split(', ')[1]
+    button_ids = session.scalars(select(Tree.qid).where(
+        Tree.pid == id).where(Tree.properties.like('<button%'))).fetchall()
+    buttons = []
+    for button_id in button_ids:
+        buttons.append(
+            (
+                session.scalar(select(Data.text).where(Data.id == button_id)),
+                button_id
+            )
+        )
+    if not buttons:
+        keyboard = ReplyKeyboardRemove()
+        return keyboard
+    if keyboard_type == '<ikb>':
+        keyboard = InlineKeyboardMarkup(row_width=1).add(*[InlineKeyboardButton(text=text, callback_data=callback_data) for text, callback_data in buttons])
+    elif keyboard_type == '<kb>':
+        keyboard = ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True, resize_keyboard=True).add(*[KeyboardButton(text=text) for text, callback_data in buttons])
+    return keyboard
+
+# ---------------------------------------------------------------------------
+
+
+@dp.message_handler(commands=['start'], state=['*'])
+async def cmd_start(message: types.Message, state: FSMContext):
+    await state.finish()
+    data = find_next()
+    if len(data['text']) > 1:
+        for text in data['text'][:-1]:
+            await message.answer(text=text)
+        text = data['text'][-1]
+    else:
+        text = data['text'][0]
+    keyboard = find_keyboard(data['id'])
+    await message.answer(text=text, reply_markup=keyboard)
+
+
+@dp.callback_query_handler()
+async def define_category(callback: types.CallbackQuery, state: FSMContext):
+    await callback.answer()
+    if int(callback.data) == 2:
+        await InfoStates.dialog.set()
+    elif int(callback.data) == 17:
+        await ComplainStates.choose_categry.set()
+    await bot.send_chat_action(chat_id=callback.message.from_user.id, action='typing')
+    data = find_next(int(callback.data))
+    if len(data['text']) > 1:
+        for text in data['text'][:-1]:
+            await callback.message.answer(text=text)
+        text = data['text'][-1]
+    else:
+        text = data['text'][0]
+    keyboard = find_keyboard(data['id'])
+    await callback.message.answer(text=text, reply_markup=keyboard)
+    async with state.proxy() as st:
+        st['prev'] = data['id']
+
+
+@dp.callback_query_handler(state=InfoStates.dialog)
+async def callback_dialog(callback: types.CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await bot.send_chat_action(chat_id=callback.message.from_user.id, action='typing')
+    data = find_next(int(callback.data))
+    if len(data['text']) > 1:
+        for text in data['text'][:-1]:
+            await callback.message.answer(text=text)
+        text = data['text'][-1]
+    else:
+        text = data['text'][0]
+    keyboard = find_keyboard(data['id'])
+    await callback.message.answer(text=text, reply_markup=keyboard)
+    async with state.proxy() as st:
+        st['prev'] = data['id']
+
+
+
+@dp.message_handler(state=InfoStates.dialog)
+async def dailog(message: types.Message, state: FSMContext):
+    await bot.send_chat_action(chat_id=message.from_user.id, action='typing')
+    async with state.proxy() as st:
+        id = st['prev']
+    children = session.scalars(select(Tree.qid).where(Tree.pid == id)).all()
+    for id in children:
+        temp = session.scalar(select(Data.id).where(Data.text == message.text).where(Data.id == id))
+        if temp:
+            break
+    data = find_next(temp)
+    if len(data['text']) > 1:
+        for text in data['text'][:-1]:
+            await message.answer(text=text)
+        text = data['text'][-1]
+    else:
+        text = data['text'][0]
+    keyboard = find_keyboard(data['id'])
+    await message.answer(text=text, reply_markup=keyboard)
+    async with state.proxy() as st:
+        st['prev'] = data['id']
+
+
+if __name__ == '__main__':
+    try:
+        executor.start_polling(dp, skip_updates=True)
+    except Exception as exception:
+        print(exception)
--- main.py
+++ main.py
@@ -1,7 +1,359 @@
-from bot import dp, start_bot
+import sqlite3
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+users = {'a1': {'password': 'pass1'},
+         'a2': {'password': 'pass2'}}
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def userLoader(username):
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+    return user
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and password == users[username]['password']:
+            user = User()
+            user.id = username
+            login_user(user)
+            return redirect('/index')
+        return 'Invalid'
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
+
+
+@app.route('/bot-flow')
+@login_required
+def show_flow():
+    tree = []
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (null, )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
+    return render_template('botFlow.html', tree=tree)
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    return render_template('index.html')
+
+@app.route('/info-tree')
+@login_required
+def info_tree():
+    tree = []
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[0][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
+    return render_template('infoTree.html', tree=tree)
+
+@app.route('/info-tree/<int:id>/change', methods=['POST', 'GET'])
+@login_required
+def changeLeaf(id):
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        props = cur.execute("""SELECT prop FROM properties WHERE category is (?) """, (2, )).fetchone()[0].split(' | ')
+        properties = []
+        for el in props:
+            if el != '<button>':
+                temp = (el, cur.execute("""SELECT text FROM properties_text WHERE prop is (?) """, (el, )).fetchone()[0])
+                properties.append(temp)
+        current_property = (cur.execute("""SELECT properties FROM tree WHERE qid IS (?) """, (id, )).fetchone()[0])
+        current_property = (current_property, (cur.execute("""SELECT text FROM properties_text WHERE prop IS (?) """, (current_property, )).fetchone()[0]))
+        data = cur.execute("""SELECT text FROM data WHERE id IS (?)""", (id, )).fetchone()
+        
+    if request.method == "POST":
+        text = request.form['text']
+        property = None
+        try:
+            if request.form['button-type'] and request.form['button-type'] != '-':
+                property = request.form['button-type']
+        except KeyError:
+            pass
+        with sqlite3.connect('database/data.db') as db:
+            cur = db.cursor()
+            db.execute("""BEGIN TRANSACTION""")
+            cur.execute("""UPDATE data
+                            SET  (text)
+                            = ((?))
+                            WHERE id IS (?) ;
+                            """, (text, id))
+            if property:
+                cur.execute("""UPDATE tree
+                                SET  (properties)
+                                = ((?))
+                                WHERE qid IS (?) ;
+                                """, (property, id))
+            db.execute("""END TRANSACTION""")
+        return redirect('/info-tree')
+    
+    return render_template('changeLeaf.html', data=data, properties=properties, current_property=current_property, id=id)
+
+
+
+
+@app.route('/info-tree/delete/<int:id>')
+@login_required
+def deleteLeaf(id):
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        db.execute("""PRAGMA foreign_keys = ON""")
+        db.execute("""BEGIN TRANSACTION""")
+        cur.execute("""DELETE FROM tree WHERE qid is (?) """, (id, ))
+        db.execute("""END TRANSACTION""")
+    return redirect('/info-tree')
+
+@app.route('/complain-tree/delete/<int:id>')
+@login_required
+def deleteComplainLeaf(id):
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        db.execute("""PRAGMA foreign_keys = ON""")
+        db.execute("""BEGIN TRANSACTION""")
+        cur.execute("""DELETE FROM tree WHERE qid is (?) """, (id, ))
+        db.execute("""END TRANSACTION""")
+    return redirect('/complain-tree')
+
+
+@app.route('/info-tree/add', methods=['POST', 'GET'])
+@login_required
+def addLeaf():
+    tree = []
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[0][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
+        props = cur.execute("""SELECT prop FROM properties WHERE category is (?) """, (cats[0][0], )).fetchone()[0].split(' | ')
+        properties = []
+        for el in props:
+            temp = (el, cur.execute("""SELECT text FROM properties_text WHERE prop is (?) """, (el, )).fetchone()[0])
+            properties.append(temp)
+        
+    if request.method == 'POST':
+        text = request.form['text']
+        property = request.form['button-type']
+        pid = request.form['pid']
+        if not text:
+            return redirect('/info-tree/add')
+        elif property == '--Выберите вид элемента--':
+            return redirect('/info-tree/add')
+        elif pid == '--Выберите элемент-родитель--':
+            return redirect('/info-tree/add')
+
+        with sqlite3.connect('database/data.db') as db:
+            cur = db.cursor()
+            cur.execute("""INSERT INTO tree (pid, properties)
+                        VALUES ((?), (?))
+                        """, (pid, property))
+            cur.execute("""INSERT INTO data (text)
+                        VALUES ((?))
+                        """, (text, ))
+            
+        return redirect('/info-tree/add')
+    return render_template('addLeaf.html', tree=tree, properties=properties)
+
+
+@app.route('/complain-tree/<int:id>/change', methods=['POST', 'GET'])
+@login_required
+def changeComplainTree(id):
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        props = cur.execute("""SELECT prop FROM properties WHERE category is (?) """, (17, )).fetchone()[0].split(' | ')
+        properties = []
+        for el in props:
+            temp = (el, cur.execute("""SELECT text FROM properties_text WHERE prop is (?) """, (el, )).fetchone()[0])
+            properties.append(temp)
+        current_property = (cur.execute("""SELECT properties FROM tree WHERE qid IS (?) """, (id, )).fetchone()[0])
+        current_property = (current_property, (cur.execute("""SELECT text FROM properties_text WHERE prop IS (?) """, (current_property, )).fetchone()[0]))
+        data = cur.execute("""SELECT text FROM data WHERE id IS (?)""", (id, )).fetchone()
+        
+    if request.method == "POST":
+        text = request.form['text']
+        property = None
+        try:
+            if request.form['button-type'] and request.form['button-type'] != '-':
+                property = request.form['button-type']
+        except KeyError:
+            pass
+        with sqlite3.connect('database/data.db') as db:
+            cur = db.cursor()
+            db.execute("""BEGIN TRANSACTION""")
+            cur.execute("""UPDATE data
+                            SET  (text)
+                            = ((?))
+                            WHERE id IS (?) ;
+                            """, (text, id))
+            if property:
+                cur.execute("""UPDATE tree
+                                SET  (properties)
+                                = ((?))
+                                WHERE qid IS (?) ;
+                                """, (property, id))
+            db.execute("""END TRANSACTION""")
+        return redirect('/complain-tree')
+    
+    return render_template('changeComplainLeaf.html', data=data, properties=properties, current_property=current_property, id=id)
+
+
+@app.route('/complain-tree')
+@login_required
+def complain_tree():
+    tree = []
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[1][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
+    return render_template('complainTree.html', tree=tree)
+
+@app.route('/complain-tree/add', methods=['POST', 'GET'])
+@login_required
+def addComplainLeaf():
+    tree = []
+    with sqlite3.connect('database/data.db') as db:
+        cur = db.cursor()
+        null = cur.execute("""SELECT qid FROM tree WHERE pid IS null""").fetchone()[0]
+        cats = cur.execute("""SELECT qid FROM tree WHERE pid is (?)""", (null, )).fetchall()
+        level_id = cur.execute("""WITH RECURSIVE
+                        cte(qid, level) AS (
+                            VALUES((?), 0)
+                            UNION ALL
+                            SELECT tree.qid, cte.level+1
+                            FROM tree JOIN cte ON tree.pid = cte.qid
+                            ORDER BY 2 DESC)
+                            SELECT level, qid FROM cte""", (cats[1][0], )).fetchall()
+        
+        level_id = [el for el in level_id]
+        tree = []
+        for el in level_id:
+            temp = cur.execute("""SELECT data.text, tree.properties FROM data, tree WHERE data.id is (?) AND tree.qid IS (?)""", (el[1], el[1])).fetchone()
+            temp = (temp[0], tuple(temp[1].split(', ')))
+            tree.append((el[0]+1, temp[0], el[1], temp[1])) # (УРОВЕНЬ, ТЕКСТ, НОМЕР, СВОЙСТВА)
+        
+        props = cur.execute("""SELECT prop FROM properties WHERE category is (?) """, (cats[1][0], )).fetchone()[0].split(' | ')
+        properties = []
+        for el in props:
+            temp = (el, cur.execute("""SELECT text FROM properties_text WHERE prop is (?) """, (el, )).fetchone()[0])
+            properties.append(temp)
+            
+        
+    if request.method == 'POST':
+        text = request.form['text']
+        property = request.form['block-type']
+        pid = request.form['pid']
+        if not text:
+            return redirect('/complain-tree/add')
+        elif property == '--Выберите вид элемента--':
+            return redirect('/complain-tree/add')
+        elif pid == '--Выберите элемент-родитель--':
+            return redirect('/complain-tree/add')
+
+        with sqlite3.connect('database/data.db') as db:
+            cur = db.cursor()
+            cur.execute("""INSERT INTO tree (pid, properties)
+                        VALUES ((?), (?))
+                        """, (pid, property))
+            cur.execute("""INSERT INTO data (text)
+                        VALUES ((?))
+                        """, (text, ))
+            
+        return redirect('/complain-tree/add')
+    return render_template('addComplainLeaf.html', tree=tree, properties=properties)
+
 
 if __name__ == '__main__':
-    try:
-        start_bot(dp)
-    except Exception as exception:
-        print(exception)
+    app.run(debug=True)
+    
